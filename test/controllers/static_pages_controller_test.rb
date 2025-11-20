dbee18c23e65da543dd39b7838258c885552beac
Максим Будько <maksimbudko@MacBook-Air-Maksim.local>
2025-11-20 20:16:07 +0500

    Eliminate repetition (solves exercise 3.1)

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,10 +1,9 @@
 require "test_helper"
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
-  end
+  #test "should get home" do
+  #assert_response :success
+  #end
 
   test "should get help" do
     get static_pages_help_url
@@ -16,4 +15,8 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should get concat" do
+    get static_pages_concat_url
+    assert_response :success 
+  end
 end
